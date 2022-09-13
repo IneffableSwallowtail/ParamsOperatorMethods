74f06edf2793815fca324cf653632e5518644fc4
A.J. Miller <ajmiller1206@gmail.com>
2022-09-13 11:15:23 -0400

    first commit

---
+++ ParamsOperatorMethods/Methods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+public static class Methods
+{
+    public static int Add(params int[] list)
+    {
+        list.Append(0);
+        int sum = list[0];
+        for (int i = 1; i < list.Length; i++)
+        {
+            sum += list[i] ;
+        }
+        return sum;
+    }
+    public static int Subtract(params int[] list)
+    {
+        list.Append(0);
+        int sum = 0;
+        sum += list[0];
+        for (int i = 1; i < list.Length; i++)
+        {
+            sum -= list[i];
+        }
+        return sum;
+    }
+    public static int Multiply(params int[] list)
+    {
+        int multiplier = 1;
+        for (int i = 0; i < list.Length; i++)
+        {
+            multiplier *= list[i];
+        }
+        return multiplier;
+    }
+    public static int Divide(params int[] list)
+    {
+        list.Append(1);
+        int divider = list[0];
+        for (int i = 1; i < list.Length; i++)
+        {
+            divider /= list[i];
+        }
+        return divider;
+    }
+    public static int Modulus(params int[] list)
+    {
+        list.Append(0); 
+        int modulus = list[0];
+        for (int i = 1; i < list.Length; i++)
+        {
+            modulus %= list[i];
+        }
+        return modulus;
+    }
+}
---
+++ ParamsOperatorMethods/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+Console.WriteLine("Sum of 1, 3, and 5: " + Methods.Add(1, 3, 5));
+Console.WriteLine("100 minus 5 and minus 70: " + Methods.Subtract(100, 5, 70));
+Console.WriteLine("5 multiplied four times: " + Methods.Multiply(5, 5, 5, 5));
+Console.WriteLine("100 divided by 5, and then by 4: " + Methods.Divide(100, 5, 4));
+Console.WriteLine("The modulus of 57 divided by 17. That number, divided by 5; the modulus of that: " + Methods.Modulus(57, 17, 5));
+Console.ReadLine();
